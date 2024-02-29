959c7b184215b735aa4fb8f4ee64c790e136dc2a
Arvid Norlander <VorpalBlade@users.noreply.github.com>
2024-02-29 21:01:14 +0100

    feat: Generate man page from command line parser

--- crates/paketkoll/build.rs
+++ crates/paketkoll/build.rs
@@ -3,22 +3,26 @@ use clap::ValueEnum;
 use clap_complete::{generate_to, Shell};
 use std::env;
 use std::io::Error;
+use std::path::PathBuf;
 
 include!("src/cli.rs");
 
 fn main() -> Result<(), Error> {
-    let outdir = match env::var_os("OUT_DIR") {
-        None => return Ok(()),
-        Some(outdir) => outdir,
-    };
+    let outdir = env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound)?;
 
     let mut cmd = Cli::command();
     for &shell in Shell::value_variants() {
         generate_to(shell, &mut cmd, "paketkoll", &outdir)?;
     }
+
+    let man = clap_mangen::Man::new(cmd);
+    let mut buffer: Vec<u8> = Default::default();
+    man.render(&mut buffer)?;
+    std::fs::write(PathBuf::from(outdir).join(man.get_filename()), buffer)?;
+
     // Outputs will be in a directory like target/release/build/paketkoll-<some-hash>/out/
     // That is unfortunate, but there doesn't seem to be a way to get a stable output directory
-    // println!("cargo:warning=shell completion files generated in: {outdir:?}");
+    // println!("cargo:warning=shell completion & man page generated in: {outdir:?}");
 
     Ok(())
 }
