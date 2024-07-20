169819f6b3b55957d64cd1a9e321967129615dac
Arvid Norlander <VorpalBlade@users.noreply.github.com>
2024-07-20 19:19:41 +0200

    feat: Extend API in rune process module significantly

--- crates/konfigkoll_script/src/plugins/process.rs
+++ crates/konfigkoll_script/src/plugins/process.rs
@@ -31,7 +31,7 @@
 
 use rune::alloc::fmt::TryWrite;
 use rune::alloc::Vec;
-use rune::runtime::{Bytes, Formatter, Shared, Value, VmResult};
+use rune::runtime::{Bytes, Formatter, Mut, Shared, Value, VmResult};
 use rune::{vm_try, Any, ContextError, Module};
 
 use std::io;
@@ -44,14 +44,38 @@ pub fn module(_stdio: bool) -> Result<Module, ContextError> {
     module.ty::<Child>()?;
     module.ty::<ExitStatus>()?;
     module.ty::<Output>()?;
+    module.ty::<Stdio>()?;
+    module.ty::<ChildStdin>()?;
+    module.ty::<ChildStdout>()?;
+    module.ty::<ChildStderr>()?;
 
     module.function_meta(Command::new)?;
     module.function_meta(Command::spawn)?;
     module.function_meta(Command::arg)?;
     module.function_meta(Command::args)?;
+    #[cfg(unix)]
+    module.function_meta(Command::arg0)?;
+    module.function_meta(Command::stdin)?;
+    module.function_meta(Command::stdout)?;
+    module.function_meta(Command::stderr)?;
+    module.function_meta(Child::stdin)?;
+    module.function_meta(Child::stdout)?;
+    module.function_meta(Child::stderr)?;
+    module.function_meta(Child::id)?;
+    module.function_meta(Child::start_kill)?;
+    module.function_meta(Child::kill)?;
+    module.function_meta(Child::wait)?;
     module.function_meta(Child::wait_with_output)?;
     module.function_meta(ExitStatus::string_display)?;
     module.function_meta(ExitStatus::code)?;
+    module.function_meta(ExitStatus::success)?;
+    module.function_meta(Stdio::null)?;
+    module.function_meta(Stdio::inherit)?;
+    module.function_meta(Stdio::piped)?;
+    module.function_meta(ChildStdin::try_into_stdio)?;
+    module.function_meta(ChildStdout::try_into_stdio)?;
+    module.function_meta(ChildStderr::try_into_stdio)?;
+
     Ok(module)
 }
 
@@ -93,6 +117,31 @@ impl Command {
         self.inner.arg(arg);
     }
 
+    #[cfg(unix)]
+    #[rune::function(instance)]
+    /// Set the first process argument, argv[0], to something other than the default executable path. (Unix only)
+    fn arg0(&mut self, arg: &str) {
+        self.inner.arg0(arg);
+    }
+
+    /// Sets configuration for the child process’s standard input (stdin) handle.
+    #[rune::function(instance)]
+    fn stdin(&mut self, stdio: Stdio) {
+        self.inner.stdin(stdio.inner);
+    }
+
+    /// Sets configuration for the child process’s standard output (stdout) handle.
+    #[rune::function(instance)]
+    fn stdout(&mut self, stdio: Stdio) {
+        self.inner.stdout(stdio.inner);
+    }
+
+    /// Sets configuration for the child process’s standard error (stderr) handle.
+    #[rune::function(instance)]
+    fn stderr(&mut self, stdio: Stdio) {
+        self.inner.stderr(stdio.inner);
+    }
+
     /// Spawn the command.
     #[rune::function(instance)]
     fn spawn(mut self) -> io::Result<Child> {
@@ -113,6 +162,98 @@ struct Child {
 }
 
 impl Child {
+    /// Attempt to take the stdin of the child process.
+    ///
+    /// Once taken this can not be taken again.
+    #[rune::function(instance)]
+    fn stdin(&mut self) -> Option<ChildStdin> {
+        let inner = match &mut self.inner {
+            Some(inner) => inner,
+            None => return None,
+        };
+        let stdin = inner.stdin.take()?;
+        Some(ChildStdin { inner: stdin })
+    }
+
+    /// Attempt to take the stdout of the child process.
+    ///
+    /// Once taken this can not be taken again.
+    #[rune::function(instance)]
+    fn stdout(&mut self) -> Option<ChildStdout> {
+        let inner = match &mut self.inner {
+            Some(inner) => inner,
+            None => return None,
+        };
+        let stdout = inner.stdout.take()?;
+        Some(ChildStdout { inner: stdout })
+    }
+
+    /// Attempt to take the stderr of the child process.
+    ///
+    /// Once taken this can not be taken again.
+    #[rune::function(instance)]
+    fn stderr(&mut self) -> Option<ChildStderr> {
+        let inner = match &mut self.inner {
+            Some(inner) => inner,
+            None => return None,
+        };
+        let stderr = inner.stderr.take()?;
+        Some(ChildStderr { inner: stderr })
+    }
+
+    /// Attempt to get the OS process id of the child process.
+    ///
+    /// This will return None after the child process has completed.
+    #[rune::function(instance)]
+    fn id(&self) -> Option<u32> {
+        match &self.inner {
+            Some(inner) => inner.id(),
+            None => None,
+        }
+    }
+
+    #[rune::function(vm_result, instance)]
+    fn start_kill(&mut self) -> io::Result<()> {
+        let inner = match &mut self.inner {
+            Some(inner) => inner,
+            None => {
+                rune::vm_panic!("already completed");
+            }
+        };
+
+        inner.start_kill()
+    }
+
+    /// Sends a signal to the child process.
+    #[rune::function(vm_result, instance, path = Self::kill)]
+    async fn kill(mut this: Mut<Self>) -> io::Result<()> {
+        let inner = match &mut this.inner {
+            Some(inner) => inner,
+            None => {
+                rune::vm_panic!("already completed");
+            }
+        };
+
+        inner.kill().await
+    }
+
+    /// Attempt to wait for the child process to exit.
+    ///
+    /// This will not capture output, use [`wait_with_output`] for that.
+    #[rune::function(vm_result, instance)]
+    async fn wait(self) -> io::Result<ExitStatus> {
+        let mut inner = match self.inner {
+            Some(inner) => inner,
+            None => {
+                rune::vm_panic!("already completed");
+            }
+        };
+
+        let status = inner.wait().await?;
+
+        Ok(ExitStatus { status })
+    }
+
     // Returns a future that will resolve to an Output, containing the exit
     // status, stdout, and stderr of the child process.
     #[rune::function(vm_result, instance)]
@@ -161,7 +302,68 @@ impl ExitStatus {
     }
 
     #[rune::function]
+    fn success(&self) -> bool {
+        self.status.success()
+    }
+
+    #[rune::function]
     fn code(&self) -> Option<i32> {
         self.status.code()
     }
 }
+
+#[derive(Any)]
+#[rune(item = ::process)]
+/// Describes what to do with a standard I/O stream for a child process when passed to the stdin, stdout, and stderr methods of Command.
+struct Stdio {
+    inner: std::process::Stdio,
+}
+
+impl Stdio {
+    /// This stream will be ignored. This is the equivalent of attaching the stream to /dev/null.
+    #[rune::function(path = Self::null)]
+    fn null() -> Self {
+        Self {
+            inner: std::process::Stdio::null(),
+        }
+    }
+
+    /// The child inherits from the corresponding parent descriptor. This is the default.
+    #[rune::function(path = Self::inherit)]
+    fn inherit() -> Self {
+        Self {
+            inner: std::process::Stdio::inherit(),
+        }
+    }
+
+    /// A new pipe should be arranged to connect the parent and child processes.
+    #[rune::function(path = Self::piped)]
+    fn piped() -> Self {
+        Self {
+            inner: std::process::Stdio::piped(),
+        }
+    }
+}
+
+macro_rules! stdio_stream {
+    ($name:ident) => {
+        #[derive(Any)]
+        #[rune(item = ::process)]
+        /// The $name stream for spawned children.
+        struct $name {
+            inner: process::$name,
+        }
+
+        impl $name {
+            #[rune::function(instance)]
+            fn try_into_stdio(self) -> Result<Stdio, std::io::Error> {
+                Ok(Stdio {
+                    inner: self.inner.try_into()?,
+                })
+            }
+        }
+    };
+}
+stdio_stream!(ChildStdin);
+stdio_stream!(ChildStdout);
+stdio_stream!(ChildStderr);
