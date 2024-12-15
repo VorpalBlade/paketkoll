aaca7499967e77ba8262b6a1263f2ea3e882cbea
Arvid Norlander <VorpalBlade@users.noreply.github.com>
2024-12-15 19:29:56 +0100

    fix(ci): Use --rm flag to podman when running integration tests

--- integration_tests/images/arch/test_runner.sh
+++ integration_tests/images/arch/test_runner.sh
@@ -5,7 +5,7 @@ set -ex
 test_dir="$1"
 bin_dir="$2"
 
-podman run \
+podman run --rm \
     --mount type=bind,src=$test_dir,target=/test_dir \
     --mount type=bind,src=$bin_dir,target=/bin_dir \
     test_img /test_runner_inner.sh
