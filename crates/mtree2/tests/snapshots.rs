2900aee35c4c6978846eebc4d6c6e414b7993524
chris-MBP <python@sachsmail.de>
2025-05-08 11:57:41 +0200

    added snapshot tests

--- crates/mtree2/tests/snapshots.rs
+++ crates/mtree2/tests/snapshots.rs
@@ -19,3 +19,7 @@ test_snapshot!(test_gedit, "examples/gedit.mtree");
 test_snapshot!(test_xterm, "examples/xterm.mtree");
 test_snapshot!(test_relative_paths, "examples/relative_paths.mtree");
 test_snapshot!(test_wrapped_lines, "examples/relative_paths_wrapped.mtree");
+test_snapshot!(test_freebsd9_flavor, "examples/test_freebsd9.mtree");
+test_snapshot!(test_mtree_flavor, "examples/test_mtree.mtree");
+#[cfg(feature = "netbsd6")]
+test_snapshot!(test_netbsd6_flavor, "examples/test_netbsd6.mtree");
