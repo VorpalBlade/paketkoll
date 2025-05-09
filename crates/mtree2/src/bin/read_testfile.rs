f8eeebe1726c2822403ae76f11795b4b5d507081
chris-MBP <python@sachsmail.de>
2025-05-09 12:03:51 +0200

    reverted from_reader_with_cwd and added_with_empty_cwd instead

--- crates/mtree2/src/bin/read_testfile.rs
+++ crates/mtree2/src/bin/read_testfile.rs
@@ -5,7 +5,7 @@ fn main() {
     let content = fs::read_to_string("relative_paths_wrapped_exceeding_root.mtree").expect("");
     // let content = fs::read_to_string("relative_paths.mtree").unwrap();
     //    let content = fs::read_to_string("test_mtree.mtree").unwrap();
-    let entries = MTree::from_reader_with_cwd(content.as_bytes(), None);
+    let entries = MTree::from_reader_with_empty_cwd(content.as_bytes());
     for entry in entries {
         // Normally you'd want to handle any errors
         let entry = entry.expect("");
--- crates/mtree2/src/lib.rs
+++ crates/mtree2/src/lib.rs
@@ -113,17 +113,27 @@ where
 
     /// The constructor function for an `MTree` instance.
     ///
-    /// This uses the provided path (specified as 'Some' variant)
-    /// or an empty Pathbuf (if set to None) as the base for relative paths.
+    /// This uses the provided path  as the base for relative paths.
     /// Relative specifications are allowed to exceed the starting level but are truncated to the root.
-    pub fn from_reader_with_cwd(reader: R, cwd: Option<PathBuf>) -> Self {
+    pub fn from_reader_with_cwd(reader: R, cwd: PathBuf) -> Self {
         Self {
             inner: BufReader::new(reader).split(b'\n'),
-            cwd: cwd.unwrap_or_default(),
+            cwd,
             default_params: Params::default(),
         }
     }
 
+    /// The constructor function for an `MTree` instance.
+    ///
+    /// This uses an empty PathBuf as the base for relative paths.
+    /// Relative specifications are allowed to exceed the starting level but are truncated to the root.
+    pub fn from_reader_with_empty_cwd(reader: R) -> Self {
+        Self {
+            inner: BufReader::new(reader).split(b'\n'),
+            cwd: PathBuf::new(),
+            default_params: Params::default(),
+        }
+    }
     /// This is a helper function to make error handling easier.
     fn next_entry(&mut self, line: io::Result<Vec<u8>>) -> Result<Option<Entry>, LineParseError> {
         let line = line?;
